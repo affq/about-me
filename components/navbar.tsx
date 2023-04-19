bc0a06615f5b00b47f16b8fbeb4ebf9e9f6d1841
affq <adrianfabisiewicz@gmail.com>
2023-04-19 13:00:50 +0200

    navbar text align right

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,9 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const options = ['home', 'kontakt'];
 
 export default function Navbar() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -13,23 +16,22 @@ export default function Navbar() {
 
   return (
     <div>
-      <Button
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-        className='ml-auto'
-      >
-            <Typography variant="button" style={{ textTransform: 'none' }}>
-                home
-            </Typography>
-      </Button>
-      <Button
-      >
-            <Typography variant="button" style={{ textTransform: 'none' }}>
-                kontakt
-            </Typography>
-      </Button>
+      <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'right',
+        alignItems: 'right',
+      }}
+    >
+      {options.map((buttonText) => (
+        <Button
+        >
+                <Typography variant="button" style={{ textTransform: 'none' }}>
+                    {buttonText}
+                </Typography>
+        </Button>
+        ))}
+        </Box>
     </div>
   );
 }
